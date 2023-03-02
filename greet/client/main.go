19fd994e4315e72e030451589067710c5793c8a9
Lucas Ocampos <lucas.ocampos@gympass.com>
2023-03-02 13:21:49 -0300

    Fixed uppercase `%V` to `%v`

--- greet/client/main.go
+++ greet/client/main.go
@@ -12,7 +12,7 @@ const address = "localhost:50051"
 func main() {
 	connection, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Fatalf("Failed to connect: %V\n", err)
+		log.Fatalf("Failed to connect: %v\n", err)
 	}
 	defer connection.Close()
 
